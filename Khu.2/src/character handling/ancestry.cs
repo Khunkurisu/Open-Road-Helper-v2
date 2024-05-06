bf29aab3c817af0186eacdf856bcd4bac88e8f60
Khunkurisu <creative.3d@live.com>
2024-05-06 10:26:42 -0500

    removed redundant feature
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- Khu.2/src/character handling/ancestry.cs
+++ Khu.2/src/character handling/ancestry.cs
@@ -4,12 +4,10 @@ namespace Bot.Characters {
 	public class Ancestry : CharacterFeatures {
 		private bool _restricted = false;
 		private List<Ability> _attributes;
-		private bool _altBoosts;
 
-		public Ancestry (string name, Helper.Rarity rarity, List<string> traits, bool restricted, List<Ability> attributes, bool altBoosts) : base (name, rarity, traits) {
+		public Ancestry (string name, Helper.Rarity rarity, List<string> traits, List<Ability> attributes, bool restricted = false) : base (name, rarity, traits) {
 			_restricted = restricted;
 			_attributes = attributes;
-			_altBoosts = altBoosts;
 		}
 
 		public bool IsRestricted {
