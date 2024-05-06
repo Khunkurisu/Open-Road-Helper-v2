619061bd7e5e9fe2e38f3f03a3523f04fe9ed8ae
Khunkurisu <creative.3d@live.com>
2024-05-06 10:24:38 -0500

    simplified character storage- was overcomplicating it
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- Khu.2/src/character handling/character.cs
+++ Khu.2/src/character handling/character.cs
@@ -11,89 +11,90 @@ namespace Bot.Characters
         private string _name;
         private string _desc;
         private string _rep;
-        private int _age;
+        private uint _age;
         private float _height;
         private float _weight;
         private int _birthDay;
         private Months _birthMonth;
         private int _birthYear;
 
-        private Ancestry _ancestry;
-        private Heritage _heritage;
-        private Class _class;
-        private int _level;
-        private Generation _generation;
-        private float _currency;
-        private int _downtime;
+        private readonly string _ancestry;
+        private readonly string _heritage;
+        private readonly string _class;
+        private readonly string _background;
+        private uint _level = 0;
+        private uint _generation = 1;
+        private float _currency = 0;
+        private int _downtime = 0;
 
-        private string _colorPref;
-        private List<string> _avatars;
-        private string _charSheet;
-        private List<string> _notes = new();
+        private List<string> _languages;
+        private Dictionary<string, uint> _skills;
+        private Dictionary<string, uint> _lore;
+        private Dictionary<string, uint> _saves;
+        private List<string> _feats;
+        private List<string> _spells;
+        private List<string> _edicts = new();
+        private List<string> _anathema = new();
+
+        private string _colorPref = "#000000";
+        private readonly List<string> _avatars = new();
+        private readonly List<string> _notes = new();
 
         private int _lastTokenTrade = 0;
         private ulong _messageId;
         private readonly long _created;
-        private long _updated;
+        private long _updated = 0;
         private int _status = 0;
 
         public Character(
-            ulong user,
+            IUser user,
             string name,
             string desc,
             string rep,
-            int age,
-            float height,
-            float weight,
-            int birthday,
-            Months birthmonth,
-            int birthyear,
-            Ancestry ancestry,
-            Heritage heritage,
-            Class charClass,
-            int level,
-            Generation generation,
-            float currency,
-            int downtime,
-            string color,
-            List<string> avatars,
-            string sheet,
-            ulong messageId
+            Dictionary<string, dynamic> data
         )
         {
             _id = Guid.NewGuid();
-            _user = user;
+            _user = user.Id;
             _name = name;
             _desc = desc;
             _rep = rep;
-            _age = age;
-            _height = height;
-            _weight = weight;
-            _ancestry = ancestry;
-            _heritage = heritage;
-            _class = charClass;
-            _level = level;
-            _currency = currency;
-            _downtime = downtime;
-            _colorPref = color;
-            _avatars = avatars;
-            _charSheet = sheet;
-            _messageId = messageId;
-            _generation = generation;
-            _birthDay = birthday;
-            _birthMonth = birthmonth;
-            _birthYear = birthyear;
             _created = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            _class = data["class"];
+            _ancestry = data["ancestry"];
+            _heritage = data["heritage"];
+            _background = data["background"];
+            _age = data["age"];
+            _currency = data["coin"];
+            _languages = data["languages"];
+            _skills = data["skills"];
+            _lore = data["lore"];
+            _saves = data["saves"];
+            _feats = data["feats"];
+            _spells = data["spells"];
+
+            if (data.ContainsKey("edicts"))
+            {
+                _edicts = data["edicts"];
+            }
+
+            if (data.ContainsKey("anathema"))
+            {
+                _anathema = data["anathema"];
+            }
         }
 
         private void SetStatus(Status status)
         {
             _status = (int)status;
+            _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
         }
 
         public void AddNote(string note)
         {
             _notes.Add(note);
+            _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
         }
 
         public void RemoveNote(int index)
@@ -101,6 +102,7 @@ namespace Bot.Characters
             if (index > 0 && index < _notes.Count - 1)
             {
                 _notes.RemoveAt(index);
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
             }
         }
 
@@ -120,62 +122,105 @@ namespace Bot.Characters
         public string Name
         {
             get { return _name; }
-            set { _name = value; }
+            set
+            {
+                _name = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
         public string Desc
         {
             get { return _desc; }
-            set { _desc = value; }
+            set
+            {
+                _desc = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
         public string Rep
         {
             get { return _rep; }
-            set { _rep = value; }
+            set
+            {
+                _rep = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
-        public int Age
+        public uint Age
         {
             get { return _age; }
-            set { _age = value; }
+            set
+            {
+                _age = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
         public float Height
         {
             get { return _height; }
-            set { _height = value; }
+            set
+            {
+                _height = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
         public float Weight
         {
             get { return _weight; }
-            set { _weight = value; }
+            set
+            {
+                _weight = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
         public int Birthday
         {
             get { return _birthDay; }
+            set
+            {
+                _birthDay = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
         public Months BirthMonth
         {
             get { return _birthMonth; }
+            set
+            {
+                _birthMonth = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
         public int BirthYear
         {
             get { return _birthYear; }
+            set
+            {
+                _birthYear = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
-        public Ancestry Ancestry
+        public string Ancestry
         {
             get { return _ancestry; }
         }
-        public Heritage Heritage
+        public string Heritage
         {
             get { return _heritage; }
         }
-        public Class Class
+        public string Class
         {
             get { return _class; }
         }
-        public Generation Generation
+        public string Background
+        {
+            get { return _background; }
+        }
+        public uint Generation
         {
             get { return _generation; }
         }
-        public int Level
+        public uint Level
         {
             get { return _level; }
         }
@@ -190,22 +235,29 @@ namespace Bot.Characters
         public string Color
         {
             get { return _colorPref; }
-            set { _colorPref = value; }
+            set
+            {
+                _colorPref = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
         public string AvatarURL
         {
             get { return _avatars[0]; }
-            set { _avatars[0] = value; }
-        }
-        public string SheetURL
-        {
-            get { return _charSheet; }
-            set { _charSheet = value; }
+            set
+            {
+                _avatars[0] = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
         public ulong MessageId
         {
             get { return _messageId; }
-            set { _messageId = value; }
+            set
+            {
+                _messageId = value;
+                _updated = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            }
         }
         public List<string> Notes
         {
