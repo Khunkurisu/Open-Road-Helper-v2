473b9c8879605632d294153fc52a41cd8ec40bd4
Khunkurisu <creative.3d@live.com>
2024-05-09 11:36:29 -0500

    now includes import type enum
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- Khu.2/src/character handling/IImportable.cs
+++ Khu.2/src/character handling/IImportable.cs
@@ -3,5 +3,13 @@ namespace Bot.Characters
     public interface IImportable
     {
         public Dictionary<string, dynamic>? GetCharacterData();
+
+        public ImportType ImportType { get; }
+    }
+
+    public enum ImportType
+    {
+        Foundry,
+        Pathbuilder
     }
 }
