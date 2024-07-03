408a2bc71a5e82e8da8fa3e7cafc1ba416ce9df2
Khunkurisu <creative.3d@live.com>
2024-07-03 03:11:12 -0500

    updated to actually reply
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- Khu.2/src/slash commands/gm management.cs
+++ Khu.2/src/slash commands/gm management.cs
@@ -20,6 +20,7 @@ namespace Bot.GameMaster
             {
                 Guild guild = Manager.GetGuild(Context.Guild.Id);
                 await guild.RefreshCharacterPosts();
+				await ReplyAsync("Character posts refreshed!");
             }
 
             [SlashCommand("quests", "Refresh all character posts with database values.")]
@@ -27,6 +28,7 @@ namespace Bot.GameMaster
             {
                 Guild guild = Manager.GetGuild(Context.Guild.Id);
                 await guild.RefreshQuestPosts();
+				await ReplyAsync("Character posts refreshed!");
             }
         }
 
@@ -38,12 +40,14 @@ namespace Bot.GameMaster
             {
                 Guild guild = Manager.GetGuild(Context.Guild.Id);
                 await guild.AddGMRole(role);
+				await ReplyAsync($"{role.Name} added as GM Role!");
             }
             [SlashCommand("remove", "Remove a role to include as GM.")]
             public async Task RemoveGMRole(IRole role)
             {
                 Guild guild = Manager.GetGuild(Context.Guild.Id);
                 await guild.RemoveGMRole(role);
+				await ReplyAsync($"{role.Name} removed from GM Roles!");
             }
         }
 
