8d9f9e519a74e4f927ab5b8e01ec2ca8ddd2c3f1
Khunkurisu <creative.3d@live.com>
2024-08-19 08:48:01 -0500

    use the id for the test server
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- Khu.2/src/SlashCommandHandler.cs
+++ Khu.2/src/SlashCommandHandler.cs
@@ -27,7 +27,8 @@ namespace Bot.Commands
                 _serviceProvider
             );
             //await _interactionService.RegisterCommandsGloballyAsync();
-            await _interactionService.RegisterCommandsToGuildAsync(1123803241793192046);
+            //await _interactionService.RegisterCommandsToGuildAsync(1123803241793192046);
+            await _interactionService.RegisterCommandsToGuildAsync(1165262696397152276);
 
             _client.InteractionCreated += async interaction =>
             {
